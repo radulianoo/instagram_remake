41f8cfbdcb7da4bdb664ae49a494ef880203d7ce
radulianoo <octav@raduliano.tech>
2023-03-22 20:39:44 +0200

    Created the project

---
+++ InstagramRemake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramRemake
+//
+//  Created by Octav Radulian on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
